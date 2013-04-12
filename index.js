b29bd77bfc0b58112d551f25035b4ae316f4aeba
Dominik Burgdörfer <dominik.burgdoerfer@mikanda.de>
2013-04-12 14:35:48 +0200

    Moved structure to new component format.

--- index.js
+++ index.js
@@ -1,15 +1,9 @@
-var backend = require('./lib/backend');
-
-/**
- * This is a backend for mistore that is used to render latex documents as pdf
- * streams.  All dependencies are fetched and written with their name to the
- * temporary directory.  When all streams are written the document is rendered
- * using pdflualatex.
- */
-
-module.exports = function (app, next) {
-  var mistore = app.require('mistore'),
-      Backend = backend(app, mistore);
-  mistore.backend.latex = Backend;
-  next();
+var package = require('./package');
+module.exports = {
+  name: package.name,
+  dependencies: [
+    require('mistore'),
+    require('mistore-ejs-latex')
+  ],
+  install: require('./lib')
 };
